ff762cd1674b1c9a61911387ac8e95ca6bd317eb
Michelle <azariahthesecond@gmail.com>
2024-03-22 17:13:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let activity = 'bathe my dog';
+console.log(saturdayFun(myActivity)); 
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+let myActivity = 'work from home';
+console.log(mondayWork(myActivity));
+
+
+function wrapAdjective(highlight = '*') {
+  return function(adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+  }
+}
+
+
+
+
